04905d073a7e1f871aa1016c6bca9229f811f2ea
Trip25 <t.gurung1995@gmail.com>
2023-04-07 15:52:44 +0100

    Ok they seem to be working so pushing so I can have a mean break >:D

--- main.test.js
+++ main.test.js
@@ -1,1 +1,33 @@
 //👉 Write your tests below here:
+import {expect, test} from '@jest/globals';
+import { getRobotNames } from './main.js';
+
+
+test("If Javascript is the only robot in the list, return an empty array", () => {
+  const robots = ['JavaScript'];
+  const actual = getRobotNames(robots);
+  const expected = [];
+  expect(actual).toStrictEqual(expected);
+});
+
+test("If Javascript is the only robot in the list-yet multiple entries, return an empty array", () => {
+    const robots = ['JavaScript', 'JavaScript', 'JavaScript'];
+    const actual = getRobotNames(robots);
+    const expected = [];
+    expect(actual).toStrictEqual(expected);
+});
+
+test("If other robots and Javascript are in the list, return the robots that are not called Javascript", () => {
+  const robots = ['Ruby', 'JavaScript', 'WallE', 'C#'];
+  const actual = getRobotNames(robots);
+  const expected = ['Ruby', 'WallE', 'C#'];
+  expect(actual).toStrictEqual(expected);
+});
+
+test('If there are no robots in the list, return an empty array', () => {
+  const robots = [];
+  const actual = getRobotNames(robots);
+  const expected = [];
+  expect(actual).toStrictEqual(expected);
+});
+
